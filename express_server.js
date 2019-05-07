54572b9153f11ecfa2fb173e5439719394d6304d
nelsonyu <nelson.tm.yu@gmail.com>
2019-05-07 23:19:32 +0000

    Changed 'var' to 'const'

--- express_server.js
+++ express_server.js
@@ -1,10 +1,13 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs"); // set ejs as view engine
 
-var urlDatabase = {
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
